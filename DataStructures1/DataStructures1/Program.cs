ba4ebbc19364b92d35c0ebde57aa0a5d0052a4f5
SamsonProjects <samson.antwi123@gmail.com>
2017-02-05 17:31:27 +0000

    Hello Commit

---
+++ DataStructures1/DataStructures1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Git commit with c# project");
+        }
+    }
+}
