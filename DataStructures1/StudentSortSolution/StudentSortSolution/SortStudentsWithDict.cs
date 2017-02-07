61c445fce0b2bc27023d7231da642bcfb48450fb
SamsonProjects <samson.antwi123@gmail.com>
2017-02-07 13:11:35 +0000

    Student sort final sol

---
+++ DataStructures1/StudentSortSolution/StudentSortSolution/SortStudentsWithDict.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSortSolution
+{
+    public class SortStudentsWithDict
+    {
+        static void Main()
+        {
+            //Read the file and build the hash-table of the courses
+            Dictionary<string, List<Student>> courses =
+                new Dictionary<string, List<Student>>();
+
+          string InFile = @"..\..\MyStudents.txt";
+        StreamReader reader = new StreamReader(InFile);
+            using (reader)
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if(line == null)
+                    {
+                        break;
+                    }
+                    string[] entry = line.Split(new char[] { '|' });
+                    string firstName = entry[0].Trim();
+                    string lastName = entry[1].Trim();
+                    string course = entry[2].Trim();
+                    List<Student> students;
+                    if(! courses.TryGetValue(course, out students)) //see if key value pair exists
+                    {
+                        //New course -> create a list of students for it
+                        students = new List<Student>();
+                        courses.Add(course, students);
+                    }
+
+                    //create  a student class
+                    Student student = new Student(firstName, lastName);
+                    students.Add(student);//add the student to the dictionary key value match
+
+                }
+            }
+
+            //Print the courses and their students
+            foreach(string course in courses.Keys)
+            {
+                Console.WriteLine("Course " + course + ":");
+                List<Student> students = courses[course]; //gets the value of the key, which is a string
+                students.Sort();
+                foreach (Student student in students)//iterate throgh list to obtain instances of students
+                {
+                    Console.WriteLine("\t{0}", student);
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ DataStructures1/StudentSortSolution/StudentSortSolution/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSortSolution
+{
+    public class Student : IComparable<Student>
+    {
+        private string firstName;
+        private string lastName;
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public int CompareTo(Student student)
+        {
+            int result = lastName.CompareTo(student.lastName);
+            if(result == 0)
+            {
+                result = firstName.CompareTo(student.firstName);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+        
+    }
+}
