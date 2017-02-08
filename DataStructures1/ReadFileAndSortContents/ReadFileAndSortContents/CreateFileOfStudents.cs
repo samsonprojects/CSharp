c681c5890f56f50caa2448457b70a133c552c539
SamsonProjects <samson.antwi123@gmail.com>
2017-02-08 13:22:30 +0000

    Created the read file

---
+++ DataStructures1/ReadFileAndSortContents/ReadFileAndSortContents/CreateFileOfStudents.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadFileAndSortContents
+{
+    public class CreateFileOfStudents
+    {
+        private const string StudentFile = @"..\..\Students.txt";
+        private SortedDictionary<string, List<Student>> Subjects =
+            new SortedDictionary<string, List<Student>>();
+
+        public CreateFileOfStudents()
+        {
+            
+        }
+
+        public void ReadStudenFile()
+        {
+            StreamReader reader = new StreamReader(StudentFile);
+            using (reader)
+            {
+                string Line;
+                while (true)
+                {
+                    Line = reader.ReadLine();
+                    if (Line == null)
+                    {
+                        break;
+                    }
+                    string[] entry = Line.Split(new char[] { '|' });
+                    string fullName = entry[0].Trim();
+                    string firstName = fullName.Split(' ')[0];
+                    string lastName = fullName.Split(' ')[1];
+                    string course = entry[1].Trim();
+
+                    //Console.WriteLine("{0}-{1}-{2}", course, firstName, lastName);
+                }
+            }
+
+        }
+
+        public void FileStudents()
+        {
+            List<Student> students;
+            if(!this.Subjects.TryGetValue)
+        }
+        static void Main()
+        {
+            CreateFileOfStudents TestCase = new CreateFileOfStudents();
+            TestCase.ReadStudenFile();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructures1/ReadFileAndSortContents/ReadFileAndSortContents/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFileAndSortContents
+{
+
+    public class Student: IComparable<Student>
+    {
+        //the Icomparable is defined so we can define the order of elements based
+        //on this solution, sort by family name and then first name
+        private string firstName;
+        private string lastName;
+
+        public Student(string FirstName, string LastName)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+        }
+
+        public int CompareTo(Student student)
+        {
+            int result = lastName.CompareTo(student.lastName);
+            if(result == 0)
+            {
+                result = firstName.CompareTo(student.firstName);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+
+    }
+}
