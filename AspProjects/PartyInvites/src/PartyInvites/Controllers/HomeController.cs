6035a77fc5e18098ec88ec4d19c4177518b900b6
SamsonProjects <samson.antwi123@gmail.com>
2017-02-16 17:46:08 +0000

    simple data entry created

--- AspProjects/PartyInvites/src/PartyInvites/Controllers/HomeController.cs
+++ AspProjects/PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
@@ -6,7 +9,39 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: store response from guest
+                Repository.AddResponse(guestResponse);
+
+                return View("Thanks", guestResponse);
+
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+            
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
