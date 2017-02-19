aafdd4b3b7ac834f0cb452effd59bd9ec6e20997
SamsonProjects <samson.antwi123@gmail.com>
2017-02-19 15:29:34 +0000

    Razor examples implemented

---
+++ LinqAndAnnonymous/ConsoleApplication1/ConsoleApplication1/SubstringExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Implement an extension method Substring(int index, int length)
+/// for the class StringBuilder that returns a new StringBuilder 
+/// and has the same functionality as the method Substring(…) of the class String. 
+/// </summary>
+
+namespace ConsoleApplication1
+{
+    public static class SubstringExtension
+    {
+        public static StringBuilder Substring(this StringBuilder str, int index, int length)
+        {
+            StringBuilder newStr = new StringBuilder();
+            try
+            {
+                
+
+                if (index + length < str.Length)
+                {
+
+                    for (int i = index; i < index + length; i++)
+                    {
+                        newStr.Append(str[i]);
+                    }
+
+                }
+                
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.Write("the index and length is out of range");
+            }
+            return newStr;
+
+
+        }
+        
+       
+        static void Main(string[] args)
+        {
+            StringBuilder helloString = new StringBuilder("Hello, Extension Methods!");
+            StringBuilder myStr = helloString.Substring(4, 7);
+            Console.WriteLine(myStr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqAndAnnonymous/ConsoleApplication2/ConsoleApplication2/IEnumerableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Implement the following extension methods for the classes,
+/// implementing the interface IEnumerable<T>: Sum(), Min(), Max(), Average(). 
+/// </summary>
+
+namespace ConsoleApplication2
+{
+    public static class IEnumerableExtensions
+    {
+        
+        public static int Sum(this IEnumerable<int> enumeration)
+        {
+            int sum = 0;
+            try
+            {
+                
+                foreach (int num in enumeration)
+                {
+                    sum += num;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new ApplicationException("Invalid enumeration values type , must be of type INT", e);
+            }
+
+            return sum;
+            
+        }
+
+        public static int Min(this IEnumerable<int> enumeration)
+        {
+            int min = 0;
+            try
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 4, 5, 6, 8, 9, 3, 2 };
+            Console.Write(numbers.Sum());
+            Console.ReadLine();
+        }
+    }
+}
